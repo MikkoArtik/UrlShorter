39879a85f506f65e74027b01a3d1314b3d4424fa
Michael <mihail.tchernov@yandex.ru>
2022-01-04 01:51:02 +0300

    Добавлено отслеживание уникальности коротких ссылок при генерации

--- dbase/tests/helper.py
+++ dbase/tests/helper.py
@@ -1,6 +1,6 @@
 from random import randint, choices
 
-from core.helper import generate_unique_link
+from core.helper import generate_unique_url
 
 ASCII_LIMITS = 41, 91
 
@@ -10,8 +10,13 @@ def generate_random_string(size=20):
 
 
 def get_duplicate_records(generate_records: list):
+    unique_short_urls = set((x[1] for x in generate_records))
     duplicate_records = []
     for item in choices(generate_records, k=len(generate_records) // 2):
-        unique_url, expiration_days = generate_unique_link(), randint(100, 300)
-        duplicate_records.append([item[0], unique_url, expiration_days])
+        short_url, expiration_days = generate_unique_url(), randint(100, 300)
+        if short_url in unique_short_urls:
+            continue
+        unique_short_urls.add(short_url)
+
+        duplicate_records.append([item[0], short_url, expiration_days])
     return duplicate_records
