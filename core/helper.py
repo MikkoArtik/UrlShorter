70e85c853c2b18cf2507b4a00d6bbcb504e03932
Michael <mihail.tchernov@yandex.ru>
2022-01-04 01:14:33 +0300

    Создан подпакет логики приложения

---
+++ core/helper.py
@@ -0,0 +1,18 @@
+import random
+from datetime import datetime, timedelta
+
+
+ALL_SYMBOL_IDS = set([x for x in range(48, 58)] + [x for x in range(97, 123)])
+GOOD_SYMBOLS = [chr(x) for x in ALL_SYMBOL_IDS]
+
+UNIQUE_LINK_SIZE = 5
+
+
+def generate_unique_link() -> str:
+    return ''.join(random.choices(GOOD_SYMBOLS, k=UNIQUE_LINK_SIZE))
+
+
+def is_valid_link(registration_datetime: datetime,
+                  expiration_days: int) -> bool:
+    end_datetime = registration_datetime + timedelta(days=expiration_days)
+    return end_datetime < datetime.now()
