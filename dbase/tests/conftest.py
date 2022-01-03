223ab351bea1222c6444d55fafa1de20171dbf36
Michael <mihail.tchernov@yandex.ru>
2022-01-04 01:49:46 +0300

    БД поднимается для каждой функции
    Добавлено отслеживание уникальности при генерации коротких ссылок

--- dbase/tests/conftest.py
+++ dbase/tests/conftest.py
@@ -8,10 +8,10 @@ from pytest import fixture
 from dbase.dbase import load_dbase_script
 
 from dbase.tests.helper import generate_random_string
-from core.helper import generate_unique_link
+from core.helper import generate_unique_url
 
 
-@fixture(scope='session')
+@fixture(scope='function')
 def create_dbase() -> Connection:
     dbase = sqlite3.connect(':memory:')
     sql_script = load_dbase_script()
@@ -23,9 +23,15 @@ def create_dbase() -> Connection:
 @pytest.fixture
 def generate_records():
     records = []
+    unique_short_urls = set()
     for i in range(1000):
         origin_url = generate_random_string()
-        short_url = generate_unique_link()
+        short_url = generate_unique_url()
+        if short_url in unique_short_urls:
+            continue
+
+        unique_short_urls.add(short_url)
+
         expiration_days = randint(1, 30)
         records.append([origin_url, short_url, expiration_days])
     return records
